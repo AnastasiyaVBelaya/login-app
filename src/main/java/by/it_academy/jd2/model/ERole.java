7ebf1ae4fe5f6f6594a959b007c7078f100e5e49
Anastasiya Belaya <anastasia-belaya@mail.ru>
2024-10-27 18:48:50 +0300

    исправлен enum - убрала лишние методы и строковые значения

--- src/main/java/by/it_academy/jd2/model/ERole.java
+++ src/main/java/by/it_academy/jd2/model/ERole.java
@@ -1,21 +1,6 @@
 package by.it_academy.jd2.model;
 
 public enum ERole {
-    ADMIN("Администратор"),
-    USER("Пользователь");
-
-    private final String displayName;
-
-    ERole(String role) {
-        this.displayName = role;
-    }
-
-    public String getRole() {
-        return displayName;
-    }
-
-    @Override
-    public String toString() {
-        return displayName;
-    }
+    ADMIN,
+    USER;
 }
